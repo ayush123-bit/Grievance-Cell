9c938cb7c576f46493506d346572c9d2cc5288f6
ayush123-bit <145470522+ayush123-bit@users.noreply.github.com>
2025-01-31 18:22:24 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://userayush:1234@cluster0.p7zfp.mongodb.net/UGI")
+mongoose.connect(process.env.MONGO)
   .then(() => {
     console.log("Connection Successful");
   })
